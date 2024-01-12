d499142105bc6b8939840f6939600a1499fa8507
MehulSartanpara <sartanparamehul@gmail.com>
2024-01-12 23:34:24 +0530

    Added static checkout text

---
+++ extensions/checkout-ui/src/Checkout.jsx
@@ -0,0 +1,29 @@
+import {
+  useApi,
+  useTranslate,
+  reactExtension,
+  Link,
+  TextBlock,
+  BlockStack,
+  View,
+} from '@shopify/ui-extensions-react/checkout';
+
+export default reactExtension(
+  'purchase.checkout.payment-method-list.render-after',
+  () => <Extension />,
+);
+
+function Extension() {
+  const translate = useTranslate();
+  const { extension } = useApi();
+
+  return (
+    <BlockStack>
+      <TextBlock>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <Link to="https://www.google.com">Lorem ipsum</Link> dolor sit amet, consectetur adipiscing elit. 
+        Sed imperdiet, velit eu condimentum aliquam, elit turpis aliquet elit, nec blandit risus arcu at nulla. <Link to="mailto:sartanparamehul@gmail.com">Lorem@example.com</Link> 
+        &nbsp;or text us at (123) 456-7890. Curabitur sit amet leo ac felis facilisis sagittis. Proin vel quam quis justo pharetra scelerisque ac id elit.
+      </TextBlock>
+    </BlockStack>
+  );
+}
